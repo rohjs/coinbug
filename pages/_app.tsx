e9981d02fc95c46c3c24f4cb46f231b71005ab0f
Kimbug <kimbug@planetariumhq.com>
2022-06-27 20:15:21 +0900

    📦 Install react-query and set up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
+import { useRef } from 'react'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import type { AppProps } from 'next/app'
 
 import 'styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = useRef(new QueryClient())
 
-export default MyApp
+  return (
+    <QueryClientProvider client={queryClient.current}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
