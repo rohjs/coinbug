5fa64ec60fdac92eedb7e6140e54f32aebc8d5a4
Kimbug <kimbug@planetariumhq.com>
2022-06-27 20:17:29 +0900

    🔧 Configure svgr

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
   reactStrictMode: true,
 }
-
-module.exports = nextConfig
